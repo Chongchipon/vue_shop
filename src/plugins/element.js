41e41ec6301f96f329d1567f7863b823849a1484
chongchipon <834977135@QQ.com>
2020-04-24 22:08:57 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -38,6 +38,8 @@ import {
     Upload
 } from 'element-ui'
 
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -72,5 +74,8 @@ Vue.use(Steps)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
+
 Vue.prototype.$confirm = MessageBox.confirm
 Vue.prototype.$message = Message
